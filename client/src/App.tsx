a11732b1f65fa4a1af31eebe74c4bdb613c81ae2
Filip Condac <filipcondac@gmail.com>
2023-06-12 17:59:57 +0100

    Added Auth0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import CommentBox from "./components/CommentBox";
 import Post from "./components/Post";
+import TopNav from "./components/TopNav";
 
 const App = () => {
   return (
     <main className="flex flex-col pt-10 bg-very-light-gray min-h-screen">
+      <TopNav />
       <Post />
       <CommentBox creator={"Julius"} />
     </main>
